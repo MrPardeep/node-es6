2932ad2bf3c8128d7a5c6d646c722525714e3bf6
MrPardeep <erpardeepjain@gmail.com>
2018-02-12 18:51:28 +0530

    unused module removed

--- controllers/uploading-controller.js
+++ controllers/uploading-controller.js
@@ -7,8 +7,8 @@ let imageUploading = (req, res) => {
     let pathImage = path.join(__dirname, "../media/", req.files[0].filename)
     console.log(pathImage)
 
-    let { filename, mimetype } = req.files[0];
-    res.send(201, { filename, mimetype, pathImage });
+    let { filename } = req.files[0];
+    res.send(201, { filename, pathImage });
 }
 
 module.exports = {
