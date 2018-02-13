7764bde63a4777cb60a573aca3636a9713814ce0
MrPardeep <erpardeepjain@gmail.com>
2018-02-13 12:09:17 +0530

    CORS enable

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(multer({ dest: './media/' }).any());
 app.use(express.static('media'));
 
 require('./routes')(app);
-// app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
