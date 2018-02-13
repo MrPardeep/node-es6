97369a5ed0ead39415e89d30f6eae9406f2783ca
MrPardeep <erpardeepjain@gmail.com>
2018-02-13 12:01:34 +0530

    userchanges

--- config/dbManager.js
+++ config/dbManager.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const DB_CONNECTION_KEY = 'mongodb://localhost/test',
-    DB_CLOUD_KEY = 'mongodb://PardeepWildnet:password@ds233218.mlab.com:33218/techie';
+    DB_CLOUD_KEY = 'mongodb://techieuser:techie@ds233218.mlab.com:33218/techie';
 
 
 module.exports = {
