a1992dd1436c8de6fe05755920f25cff64d01bdd
MrPardeep <erpardeepjain@gmail.com>
2018-02-13 12:08:10 +0530

    CORS enable

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     multer = require('multer'),
+    cors = require('cors'),
     logger = require('morgan');
 
 
@@ -20,4 +21,11 @@ app.use(multer({ dest: './media/' }).any());
 app.use(express.static('media'));
 
 require('./routes')(app);
+// app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 module.exports = app;
